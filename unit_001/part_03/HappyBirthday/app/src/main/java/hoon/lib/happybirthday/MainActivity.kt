2ac1d4561cc25ce4ea7c35d401286cd27ff69429
boris920308 <jhoonj0308@gmail.com>
2025-06-04 17:33:14 +0900

    feat. font size, lineHeight 적용

--- unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
+++ unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import hoon.lib.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +28,9 @@ fun GreetingText(
     modifier: Modifier = Modifier
 ) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
     )
 
 }
