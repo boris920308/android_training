6aaf90ab0920c9410e25a3f0981b91e3319dce99
boris920308 <jhoonj0308@gmail.com>
2025-06-04 17:43:14 +0900

    feat. Column을 이용해 요소 정렬

--- unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
+++ unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,15 +30,17 @@ fun GreetingText(
     from: String,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
 }
 
 
