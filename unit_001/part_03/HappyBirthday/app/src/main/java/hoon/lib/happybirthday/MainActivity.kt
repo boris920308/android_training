ddf6662d1bfd886f78057b63b848d723dea185d2
boris920308 <jhoonj0308@gmail.com>
2025-06-04 18:50:53 +0900

    feat. 텍스트 정렬 도전과제

--- unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
+++ unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
@@ -67,7 +67,8 @@ fun GreetingText(
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally),
+
         )
     }
 }
