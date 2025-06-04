9d57135f4b50d8481a687ad2368de6c37ec360f3
boris920308 <jhoonj0308@gmail.com>
2025-06-04 18:48:14 +0900

    feat. String 리소스로 변경

--- unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
+++ unit_001/part_03/HappyBirthday/app/src/main/java/hoon/lib/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,8 +36,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -101,8 +102,8 @@ fun GreetingImage(
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Emma"
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text),
         )
     }
 }
