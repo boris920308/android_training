5a8f9c5eb8b21a3592e18da57ea0f7f4822b3db4
boris920308 <jhoonj0308@gmail.com>
2025-06-05 11:34:58 +0900

    feat. CompletedImage() 추가

--- unit_001/part_05_task_manager/app/src/main/java/hoon/lib/part_05_task_manager/MainActivity.kt
+++ unit_001/part_05_task_manager/app/src/main/java/hoon/lib/part_05_task_manager/MainActivity.kt
@@ -3,11 +3,36 @@ package hoon.lib.part_05_task_manager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            TaskManager()
         }
     }
+}
+
+@Composable
+fun TaskManager() {
+    CompletedImage()
+}
+
+@Composable
+fun CompletedImage() {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskManagerPreview() {
+    TaskManager()
 }
