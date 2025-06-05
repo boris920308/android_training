7943e9545a8b3b57e9240140d008fab6cf1dfcca
boris920308 <jhoonj0308@gmail.com>
2025-06-05 11:00:26 +0900

    feat. 첫번째 Text 컴포저블 추가

--- unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
+++ unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
@@ -3,38 +3,66 @@ package hoon.lib.part_05
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TopImage()
+            ComposeTutorial()
         }
     }
 
     @Composable
-    fun TopImage(
+    fun ComposeTutorial(
         modifier: Modifier = Modifier
     ) {
+        Column(
+            modifier = modifier
+                .systemBarsPadding()
+        ) {
+            TopImage()
+            TitleText()
+        }
+    }
+
+
+    @Composable
+    fun TopImage() {
         val image = painterResource(R.drawable.bg_compose_background)
 
         Image(
             painter = image,
             contentDescription = null,
-            modifier = modifier
-                .systemBarsPadding()
+        )
+    }
+
+    @Composable
+    fun TitleText() {
+        Text(
+            text = stringResource(R.string.title_text),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
         )
     }
 
     @Preview(showBackground = true)
     @Composable
     fun MyPreView() {
-        TopImage()
+        ComposeTutorial()
     }
 }
