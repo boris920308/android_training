4f34682e397a2a1950c33b2869db94ccdf9ad20e
boris920308 <jhoonj0308@gmail.com>
2025-06-05 10:08:54 +0900

    feat. 상단 image 추가

--- unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
+++ unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
@@ -3,20 +3,32 @@ package hoon.lib.part_05
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import hoon.lib.part_05.ui.theme.Part_05Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            TopImage()
         }
+    }
+
+    @Composable
+    fun TopImage() {
+        val image = painterResource(R.drawable.bg_compose_background)
+
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun MyPreView() {
+        TopImage()
     }
 }
