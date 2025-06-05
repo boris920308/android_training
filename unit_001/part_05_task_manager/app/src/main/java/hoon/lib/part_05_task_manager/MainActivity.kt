86d63aa63f835a6168caf4767e1d38b627a0d39f
boris920308 <jhoonj0308@gmail.com>
2025-06-05 11:17:49 +0900

    init. create part05_task_manager

---
+++ unit_001/part_05_task_manager/app/src/main/java/hoon/lib/part_05_task_manager/MainActivity.kt
@@ -0,0 +1,13 @@
+package hoon.lib.part_05_task_manager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
