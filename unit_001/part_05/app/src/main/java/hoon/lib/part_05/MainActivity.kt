b123127740c1af9bf6add07691f6e176a02040cb
boris920308 <jhoonj0308@gmail.com>
2025-06-05 11:03:26 +0900

    feat. 두번째 Text 컴포저블 추가

--- unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
+++ unit_001/part_05/app/src/main/java/hoon/lib/part_05/MainActivity.kt
@@ -3,17 +3,16 @@ package hoon.lib.part_05
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +35,7 @@ class MainActivity : ComponentActivity() {
         ) {
             TopImage()
             TitleText()
+            SecondText()
         }
     }
 
@@ -57,6 +57,19 @@ class MainActivity : ComponentActivity() {
             fontSize = 24.sp,
             modifier = Modifier
                 .padding(16.dp)
+        )
+    }
+
+    @Composable
+    fun SecondText() {
+        Text(
+            text = stringResource(R.string.second_text),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                )
         )
     }
 
