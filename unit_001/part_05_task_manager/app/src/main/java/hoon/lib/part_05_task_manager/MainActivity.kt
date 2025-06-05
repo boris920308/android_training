dc8da22cefdc45822dc524bea6fefc06f6e3135b
boris920308 <jhoonj0308@gmail.com>
2025-06-05 11:45:20 +0900

    feat. CompletedImage가 가운데 정렬되도록 수정

--- unit_001/part_05_task_manager/app/src/main/java/hoon/lib/part_05_task_manager/MainActivity.kt
+++ unit_001/part_05_task_manager/app/src/main/java/hoon/lib/part_05_task_manager/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -18,8 +23,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TaskManager() {
-    CompletedImage()
+fun TaskManager(
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        CompletedImage()
+    }
 }
 
 @Composable
