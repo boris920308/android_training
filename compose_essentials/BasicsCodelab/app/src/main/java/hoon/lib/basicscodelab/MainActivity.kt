bf103151f2975a0cfdaae482adb141332105055e
boris920308 <jhoonj0308@gmail.com>
2025-07-21 22:30:06 +0900

    feat. remember를 이용한 상태생성, 클릭이벤트 생성

--- compose_essentials/BasicsCodelab/app/src/main/java/hoon/lib/basicscodelab/MainActivity.kt
+++ compose_essentials/BasicsCodelab/app/src/main/java/hoon/lib/basicscodelab/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +51,7 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -61,9 +64,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = {}
+                onClick = {
+                    expanded.value = !expanded.value
+                }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
