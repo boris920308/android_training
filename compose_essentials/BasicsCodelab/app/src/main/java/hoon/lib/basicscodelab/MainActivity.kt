18348bd2f4c8da100fcb74b2302bc2ee139f41d3
boris920308 <jhoonj0308@gmail.com>
2025-07-21 23:12:32 +0900

    feat. 앱의 스타일 지정 및 테마 설정. 텍스트 스타일 지정

--- compose_essentials/BasicsCodelab/app/src/main/java/hoon/lib/basicscodelab/MainActivity.kt
+++ compose_essentials/BasicsCodelab/app/src/main/java/hoon/lib/basicscodelab/MainActivity.kt
@@ -119,7 +119,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium)
             }
             ElevatedButton(
                 onClick = {
