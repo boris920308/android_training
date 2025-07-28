ffe1740ab74b3f8082b250497817aac8ef55ee42
boris920308 <jhoonj0308@gmail.com>
2025-07-28 23:48:06 +0900

    feat. SearchBar 및 SearchBarPreview 생성

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import hoon.codelab.mysoothe.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,5 +19,28 @@ class MainActivity : ComponentActivity() {
             MySootheTheme {
             }
         }
+    }
+}
+
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFF5F0EE
+)
+@Composable
+fun SearchBarPreview() {
+    MySootheTheme {
+        SearchBar(
+            Modifier.padding(
+                8.dp
+            )
+        )
     }
 }
