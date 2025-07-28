a1cfa88f61f1a401e71ad9aa7a293cf4dd1b2d01
boris920308 <jhoonj0308@gmail.com>
2025-07-29 00:06:24 +0900

    feat. AlignYourBodyElement에 Image, Text 추가

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,7 +66,15 @@ fun SearchBar(
 fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.hoon_sample_00),
+            contentDescription = null
+        )
+        Text(text = stringResource(R.string.hoon_sample_string))
+    }
 }
 
 @Preview(
