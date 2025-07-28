02bae99ce67957b013ede2e23ddd036d523b7b6a
boris920308 <jhoonj0308@gmail.com>
2025-07-28 23:53:33 +0900

    feat. SearchBar/TextField에 Icon 삽입

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -33,6 +36,12 @@ fun SearchBar(
     TextField(
         value = "",
         onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
