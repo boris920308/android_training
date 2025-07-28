fb98ada7cc843cc8c6676828d646629f59d43fb0
boris920308 <jhoonj0308@gmail.com>
2025-07-28 23:55:51 +0900

    feat. SearchBar/TextField color 지정, placeholder 지정

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -10,9 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hoon.codelab.mysoothe.ui.theme.MySootheTheme
@@ -41,6 +45,13 @@ fun SearchBar(
                 imageVector = Icons.Default.Search,
                 contentDescription = null
             )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
         },
         modifier = modifier
             .fillMaxWidth()
