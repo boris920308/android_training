d634e19db43c56d5616377e0b53e2fcf0978dcdd
boris920308 <jhoonj0308@gmail.com>
2025-08-07 15:21:34 +0900

    feat. MySootheAppPortrait에 Scaffold, HomeScreen 추가

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -269,7 +270,13 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 
 @Composable
 fun MySootheAppPortrait() {
-
+    MySootheTheme {
+        Scaffold(
+            bottomBar = { SootheBottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 100)
