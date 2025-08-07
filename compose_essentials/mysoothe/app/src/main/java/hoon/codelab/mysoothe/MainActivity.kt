d83416364729dc4791adcf2db8481add432f8f94
boris920308 <jhoonj0308@gmail.com>
2025-08-07 15:33:37 +0900

    feat. SootheNavigationRail 에 NavigationRail 및 NavigationRailItem 추가

--- compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
+++ compose_essentials/mysoothe/app/src/main/java/hoon/codelab/mysoothe/MainActivity.kt
@@ -36,6 +36,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -231,7 +233,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
     NavigationBar(
-        containerColor =  MaterialTheme.colorScheme.surfaceVariant,
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
         modifier = Modifier
     ) {
         NavigationBarItem(
@@ -270,7 +272,40 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 
 @Composable
 private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+    ) {
+        Column(
+        ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+                },
+                selected = true,
+                onClick = {}
+            )
 
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_profile))
+                },
+                selected = true,
+                onClick = {}
+            )
+
+        }
+    }
 }
 
 @Composable
@@ -301,6 +336,7 @@ private fun MySootheAppPortraitPreview() {
 private fun SootheBottomNavigationPreview() {
     MySootheTheme { SootheBottomNavigation() }
 }
+
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 100)
 @Composable
 fun ScreenContentPreview() {
