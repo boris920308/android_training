cecb930d7a4e546573fb54277be82b7f377b308b
boris920308 <jhoonj0308@gmail.com>
2025-08-10 15:36:45 +0900

    feat. WaterCounter Composable에 count에 remember를 사용하여 리컴포지션간에 값이 유지되도록 변경

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -16,7 +17,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        val count: MutableState<Int> = mutableStateOf(0)
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
         Text(
             text = "You've had ${count.value} glasses.",
         )
