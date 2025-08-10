4405d8433678b3aa103a8ced3e860714785cc8b4
boris920308 <jhoonj0308@gmail.com>
2025-08-10 15:39:41 +0900

    feat. WaterCounter Composable에 count에 by 키워드를 사용

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
@@ -6,8 +6,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -17,12 +19,12 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
         Text(
-            text = "You've had ${count.value} glasses.",
+            text = "You've had ${count} glasses.",
         )
         Button(
-            onClick = { count.value++ },
+            onClick = { count++ },
             Modifier.padding(top = 8.dp)
         ) {
             Text("Add one")
