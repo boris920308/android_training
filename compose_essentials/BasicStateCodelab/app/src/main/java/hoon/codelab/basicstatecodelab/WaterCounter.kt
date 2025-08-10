91a5c1b0a7ba11d441c849351ab109b3531191e6
boris920308 <jhoonj0308@gmail.com>
2025-08-10 15:28:04 +0900

    feat. WaterCounter Composable에 Button 추가 및 event 작성

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package hoon.codelab.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,9 +10,19 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
+
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        var count = 0
+        Text(
+            text = "You've had $count glasses.",
+        )
+        Button(
+            onClick = { count++ }, // click event가 발생하더라도 ui를 다시 그리지 않아 화면에 표시되는 count는 변하지 않음
+            Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
+        }
+    }
 }
