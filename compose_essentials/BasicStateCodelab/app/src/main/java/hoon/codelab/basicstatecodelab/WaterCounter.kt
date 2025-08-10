fe623cfbfcad7a846d9f527704f36179bfe240a3
boris920308 <jhoonj0308@gmail.com>
2025-08-10 15:34:53 +0900

    feat. WaterCounter Composable에 count를 MutableState로 변경하여 리컴포지션 트리거하도록 변경

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -14,12 +16,12 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        var count = 0
+        val count: MutableState<Int> = mutableStateOf(0)
         Text(
-            text = "You've had $count glasses.",
+            text = "You've had ${count.value} glasses.",
         )
         Button(
-            onClick = { count++ }, // click event가 발생하더라도 ui를 다시 그리지 않아 화면에 표시되는 count는 변하지 않음
+            onClick = { count.value++ },
             Modifier.padding(top = 8.dp)
         ) {
             Text("Add one")
