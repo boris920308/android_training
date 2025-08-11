194ca74218e78f82dd0b46bf2d999862be737377
boris920308 <jhoonj0308@gmail.com>
2025-08-11 15:51:54 +0900

    refactor. WellnessTask 수정

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WellnessTaskItem.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WellnessTaskItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -63,8 +64,10 @@ fun WellnessTaskItemPreview() {
     }
 }
 
-data class WellnessTask(
+class WellnessTask(
     val id: Int,
     val label: String,
-    var checked: Boolean = false
-)
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
