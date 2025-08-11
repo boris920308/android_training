34de0307ee06b8473ed244aad6cbc0b36513c4ea
boris920308 <jhoonj0308@gmail.com>
2025-08-11 11:23:21 +0900

    feat. WellnessTaskItem Composable에 checkbox 추가

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WellnessTaskItem.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WellnessTaskItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -17,15 +18,25 @@ import hoon.codelab.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChanged: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
+        )
+
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChanged
         )
 
         IconButton(onClick = onClose) {
@@ -37,5 +48,5 @@ fun WellnessTaskItem(
 @Preview(showBackground = true)
 @Composable
 fun WellnessTaskItemPreview() {
-    BasicStateCodelabTheme { WellnessTaskItem("WellnessTask", onClose = {}, Modifier) }
+    BasicStateCodelabTheme { WellnessTaskItem("WellnessTask", checked = true, onCheckedChanged = {}, onClose = {}, Modifier) }
 }
