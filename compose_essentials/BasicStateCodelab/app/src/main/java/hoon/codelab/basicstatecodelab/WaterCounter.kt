7eae39db72d50c16cf18ee01ab9383bfd717e526
boris920308 <jhoonj0308@gmail.com>
2025-08-11 10:23:05 +0900

    refactor. WaterCounter Composable을 StatelessCounter, StatefulCounter로 리팩토링

--- compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
+++ compose_essentials/BasicStateCodelab/app/src/main/java/hoon/codelab/basicstatecodelab/WaterCounter.kt
@@ -10,25 +10,39 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import hoon.codelab.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+
+        Button(
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text("Add one")
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun WaterCounterPreview() {
-    BasicStateCodelabTheme { WaterCounter() }
+fun StatefulCounter(
+    modifier: Modifier = Modifier
+) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(
+        count,
+        { count++ },
+        modifier
+    )
 }
