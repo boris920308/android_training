7d246008351f20cbb4c5957d44f0bf51e17694d1
boris920308 <jhoonj0308@gmail.com>
2025-08-26 23:49:14 +0900

    feat. HomeMenuListItem. padding 추가, 가운데 정렬되도록 수정

--- MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
+++ MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
@@ -2,14 +2,17 @@ package hoon.example.mycompose.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeMenuListItem(
@@ -21,6 +24,8 @@ fun HomeMenuListItem(
     Column(
         modifier = modifier
             .clickable { onClick() }
+            .padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
             imageVector = icon,
