c28e0e68dcfb8a2fe67dfca84ee1b291d3fe694d
boris920308 <jhoonj0308@gmail.com>
2025-08-26 23:48:01 +0900

    feat. HomeMenuListItem를 재사용 가능하도록 변경

--- MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
+++ MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
@@ -1,5 +1,6 @@
 package hoon.example.mycompose.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -7,19 +8,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeMenuListItem(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    label: String,
+    onClick: () -> Unit = {}
 ) {
-    Column {
+    Column(
+        modifier = modifier
+            .clickable { onClick() }
+    ) {
         Icon(
-            imageVector = Icons.Default.Home,
-            contentDescription = "homeItem"
+            imageVector = icon,
+            contentDescription = label
         )
         Text(
-            text = "home"
+            text = label
         )
     }
 
@@ -28,5 +36,8 @@ fun HomeMenuListItem(
 @Preview(showBackground = true)
 @Composable
 fun HomeMenuListItemPreview() {
-    HomeMenuListItem()
+    HomeMenuListItem(
+        icon = Icons.Default.Home,
+        label = "Home",
+    )
 }
