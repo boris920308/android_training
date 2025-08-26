e004f9c01fc7a0cbd2a381d5d2a2dc52ad7aca5c
boris920308 <jhoonj0308@gmail.com>
2025-08-26 23:55:05 +0900

    chore. HomeMenuItem dummy data 생성

---
+++ MyCompose/app/src/main/java/hoon/example/mycompose/utils/DummyUtils.kt
@@ -0,0 +1,22 @@
+package hoon.example.mycompose.utils
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import hoon.example.mycompose.data.HomeMenuItem
+
+fun getDummyHomeMenuItem(): List<HomeMenuItem> {
+    val homeMenuItems = listOf(
+        HomeMenuItem(Icons.Default.Home, "Home"),
+        HomeMenuItem(Icons.Default.Settings, "Settings"),
+        HomeMenuItem(Icons.Default.Favorite, "Favorite"),
+        HomeMenuItem(Icons.Default.Info, "Info"),
+        HomeMenuItem(Icons.Default.Person, "Profile"),
+        HomeMenuItem(Icons.Default.Notifications, "Notice")
+    )
+    return homeMenuItems
+}
