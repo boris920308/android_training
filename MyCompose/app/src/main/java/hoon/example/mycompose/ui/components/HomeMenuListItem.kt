ebe9e5c81e6b422f459750e645a716a1662cd37d
boris920308 <jhoonj0308@gmail.com>
2025-08-26 23:42:26 +0900

    feat. Card 제거

--- MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
+++ MyCompose/app/src/main/java/hoon/example/mycompose/ui/components/HomeMenuListItem.kt
@@ -1,10 +1,8 @@
 package hoon.example.mycompose.ui.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,19 +13,16 @@ import androidx.compose.ui.tooling.preview.Preview
 fun HomeMenuListItem(
     modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier
-    ) {
-        Column {
-            Icon(
-                imageVector = Icons.Default.Home,
-                contentDescription = "homeItem"
-            )
-            Text(
-                text = "home"
-            )
-        }
+    Column {
+        Icon(
+            imageVector = Icons.Default.Home,
+            contentDescription = "homeItem"
+        )
+        Text(
+            text = "home"
+        )
     }
+
 }
 
 @Preview(showBackground = true)
