ddc307947f78224592e877bf58d1f293ae6b5c28
boris920308 <jhoonj0308@gmail.com>
2025-09-21 11:35:22 +0900

    feat. SettingsScreen 추가

---
+++ MyCompose/app/src/main/java/hoon/example/mycompose/ui/screens/settings/SettingsScreen.kt
@@ -0,0 +1,34 @@
+package hoon.example.mycompose.ui.screens.settings
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingsScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+
+            text = "SettingsScreen"
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen()
+}
