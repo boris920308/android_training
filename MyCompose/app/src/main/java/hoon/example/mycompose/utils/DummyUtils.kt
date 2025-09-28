2eba50ed6212fac4217e15f8607bb63c2c84ad98
boris920308 <jhoonj0308@gmail.com>
2025-09-28 21:31:52 +0900

    feat. HomeMenuItem 순서 변경

--- MyCompose/app/src/main/java/hoon/example/mycompose/utils/DummyUtils.kt
+++ MyCompose/app/src/main/java/hoon/example/mycompose/utils/DummyUtils.kt
@@ -15,13 +15,13 @@ import hoon.example.mycompose.ui.navigation.Screen
 fun getDummyHomeMenuItem(): List<HomeMenuItem> {
     val homeMenuItems = listOf(
         HomeMenuItem(Icons.Default.Home, "Home", Screen.Home),
+        HomeMenuItem(Icons.Outlined.DateRange, "Class", Screen.ClassList),
         HomeMenuItem(Icons.Default.Settings, "Settings", Screen.Settings),
         HomeMenuItem(Icons.Default.Favorite, "Favorite", Screen.Favorite),
         HomeMenuItem(Icons.Default.Info, "Info", Screen.Info),
         HomeMenuItem(Icons.Default.Person, "Profile", Screen.Profile),
         HomeMenuItem(Icons.Default.Notifications, "Notice", Screen.Notice),
-        HomeMenuItem(Icons.Default.Add, "Add", Screen.Add),
-        HomeMenuItem(Icons.Outlined.DateRange, "Class", Screen.ClassList)
+        HomeMenuItem(Icons.Default.Add, "Add", Screen.Add)
     )
     return homeMenuItems
 }
