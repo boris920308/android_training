5230db04f167785d00a81b47bb7f373b71a475b3
boris920308 <jhoonj0308@gmail.com>
2025-09-28 14:26:07 +0900

    feat. 각 화면 이동 구현

--- MyCompose/app/src/main/java/hoon/example/mycompose/ui/MyComposeApp.kt
+++ MyCompose/app/src/main/java/hoon/example/mycompose/ui/MyComposeApp.kt
@@ -15,8 +15,13 @@ import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import hoon.example.mycompose.data.HomeMenuItem
 import hoon.example.mycompose.ui.navigation.Screen
+import hoon.example.mycompose.ui.screens.favorite.FavoriteScreen
 import hoon.example.mycompose.ui.screens.home.HomeScreen
+import hoon.example.mycompose.ui.screens.info.InfoScreen
+import hoon.example.mycompose.ui.screens.notice.NoticeScreen
+import hoon.example.mycompose.ui.screens.profile.ProfileScreen
 import hoon.example.mycompose.ui.screens.settings.SettingsScreen
 import hoon.example.mycompose.utils.getDummyHomeMenuItem
 
@@ -48,7 +53,8 @@ fun MyComposeAppContent() {
                 HomeScreen(
                     homeMenuItem = getDummyHomeMenuItem(),
                 ) { clickedItem ->
-                    if (clickedItem.screen == Screen.Home || clickedItem.screen == Screen.Settings) {
+//                    if (clickedItem.screen == Screen.Home || clickedItem.screen == Screen.Settings) {
+                    if (isNavigateHomeMenu(clickedItem)) {
                         // 구현된 화면에서만 이동
                         navController.navigate(clickedItem.screen.route)
                     }
@@ -58,8 +64,32 @@ fun MyComposeAppContent() {
             composable(Screen.Settings.route) {
                 SettingsScreen()
             }
+            composable(Screen.Favorite.route) {
+                FavoriteScreen()
+            }
+            composable(Screen.Info.route) {
+                InfoScreen()
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+            composable(Screen.Notice.route) {
+                NoticeScreen()
+            }
         }
     }
+}
+
+fun isNavigateHomeMenu(item: HomeMenuItem): Boolean{
+    val navigableScreens = setOf(
+        Screen.Home,
+        Screen.Settings,
+        Screen.Favorite,
+        Screen.Info,
+        Screen.Profile,
+        Screen.Notice
+    )
+    return item.screen in navigableScreens
 }
 
 
